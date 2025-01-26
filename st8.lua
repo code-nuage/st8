bb8c167cf2102fd2132beb3bcf0dac546f0e9125
code-nuage <eloi.hariot@protonmail.com>
2025-01-26 19:21:34 +0100

    Adds missing features ?

--- st8.lua
+++ st8.lua
@@ -1,19 +1,59 @@
+--[[
+MIT License
+
+Copyright (c) 2024 Eloi Nuage Hariot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
 local st8 = {}
 st8.__index = st8
+st8.states = {}
+st8.states.__index = st8.states
 st8.actives = {}
 
 local function check_actives()
     if not next(st8.actives) then
-        error("No active gamestate yet.")
+        error("No active gamestate available.")
     end
 end
 
 function st8:new(name)
-    local instance = setmetatable({}, st8)
+    local instance = setmetatable({}, st8.states)
 
     instance.name = name
+    instance.state = "PLAY"
 
     return instance
+end
+
+function st8.states:pause()
+    self.state = "PAUSE"
+end
+
+function st8.states:resume()
+    self.state = "PLAY"
+end
+
+function st8.states:switch()
+    st8.pop()
+    st8.push(self)
 end
 
 function st8.push(state)
@@ -29,11 +69,15 @@ function st8.pop()
     st8.actives[#st8.actives] = nil
 end
 
+function st8.clear()
+    st8.actives = {}
+end
+
 function st8.update(dt)
     check_actives()
 
     for _, state in ipairs(st8.actives) do
-        if type(state.update) == "function" then state:update(dt) end
+        if type(state.update) == "function" and state.state == "PLAY" then state:update(dt) end
     end
 end
 
